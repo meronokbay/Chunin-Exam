ec905556ebd8b546bc086908810031abcb02ce28
Meron Ogbai <okbaymeron@gmail.com>
2021-03-29 01:41:59 +0300

    Add tracker validations and specs

--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -2,4 +2,6 @@ class Tracker < ApplicationRecord
   PLATFORMS = Set['android', 'ios', 'windows', 'mac', 'linux'].freeze
   belongs_to :url
   scope :with_platform, ->(platform) { where("platform = ?", platform)}
+  validates :url_id, presence: true
+  validates :platform, presence: true, inclusion: { in: PLATFORMS + ['other'] }
 end
