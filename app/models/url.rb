d7ad9c06e039bdf82250842dac2c9045708749c6
Meron Ogbai <okbaymeron@gmail.com>
2021-03-28 17:30:48 +0300

    Add Url validations, callback, and specs

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,2 +1,14 @@
 class Url < ApplicationRecord
+  # url regex copied from https://urlregex.com/
+  url_regex = %r{\A(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:/[^\s]*)?\z}i
+  validates :full_url, presence: true, format: { with: url_regex }
+  validates :url_digest, presence: true, uniqueness: true
+
+  before_validation :digest_url
+
+  private
+
+  def digest_url
+    self.url_digest = Digest::SHA256.hexdigest("#{Time.now}-#{rand}-#{full_url}")[0..6]
+  end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "doesn't validate url if full_url isn't present" do
+    url = Url.create()
+    expect(url.valid?).to be false
+  end
+
+  it "doesn't validate url if full_url isn't a proper url string" do
+    url = Url.create(full_url: 'example')
+    expect(url.valid?).to be false
+  end
+
+  it "doesn't validate url if full_url doesn't have a schema" do
+    url = Url.create(full_url: 'example.com')
+    expect(url.valid?).to be false
+  end
+
+  it "doesn't validate url if full_url has an invalid schema" do
+    url = Url.create(full_url: 'fff://example.com')
+    expect(url.valid?).to be false
+  end
+
+  it "validates url if full_url is present and has a valid schema" do
+    url = Url.create(full_url: 'https://example.com')
+    expect(url.valid?).to be true
+  end
 end
