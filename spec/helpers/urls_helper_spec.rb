aac8a96c4fdbc13dbc15d7a8c75c31c4d7a50fb7
Meron Ogbai <okbaymeron@gmail.com>
2021-03-29 01:55:37 +0300

    Add urls helper specs

--- spec/helpers/urls_helper_spec.rb
+++ spec/helpers/urls_helper_spec.rb
@@ -1,15 +1,37 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the UrlsHelper. For example:
-#
-# describe UrlsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-RSpec.describe UrlsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe UrlsHelper, type: :helper do  
+  context 'counts returned by visits_count' do
+    before(:example) do
+      @url = Url.create(full_url: 'http://example.com')
+      @url.trackers.create(platform: 'linux')
+      @url.trackers.create(platform: 'mac')
+      @url.trackers.create(platform: 'windows')
+    end
+
+    it 'total count is 3' do
+      expect(visits_count(@url)[:total]).to be 3
+    end
+
+    it 'linux count is 1' do
+      expect(visits_count(@url)[:linux]).to be 1
+    end
+
+    it 'windows count is 1' do
+      expect(visits_count(@url)[:windows]).to be 1
+    end
+
+    it 'android count is 0' do
+      expect(visits_count(@url)[:android]).to be 0
+    end
+
+    it 'ios count is 0' do
+      expect(visits_count(@url)[:ios]).to be 0
+    end
+
+    it 'other count is 1' do
+      @url.trackers.create(platform: 'other')
+      expect(visits_count(@url)[:other]).to be 1
+    end
+  end
 end
