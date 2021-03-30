0010c776ae5afa63de89c40eea12876be690f499
Meron Ogbai <okbaymeron@gmail.com>
2021-03-31 01:54:40 +0300

    Refactor flash messages and header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_alert_message
+    content_tag('div', alert, class: 'alert alert-danger') if alert
+  end
+
+  def flash_notice_message
+    content_tag('div', notice, class: 'alert alert-info') if notice
+  end
 end
