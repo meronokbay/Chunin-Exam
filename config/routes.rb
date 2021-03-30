cdfdb113f1f590c6cec6a803d6adf21204c82a7a
Meron Ogbai <okbaymeron@gmail.com>
2021-03-31 00:38:20 +0300

    Generate devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'urls#new'
+  devise_for :users
   resources :urls, only: [:new, :create]
   get '/:url_digest', to: 'urls#show', as: 'shortened_link'
 end
