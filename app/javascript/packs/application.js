8ef8caa8a1957e4f29766844682a518b38101246
Meron Ogbai <okbaymeron@gmail.com>
2021-03-30 11:49:12 +0300

    Add chartkick

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "controllers"
+import "chartkick"
+import "chart.js"
 
 Rails.start()
 Turbolinks.start()
