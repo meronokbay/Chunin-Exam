f259236ec0c38d2101c2a69337541d2b3cec5733
Meron Ogbai <okbaymeron@gmail.com>
2021-04-01 02:42:35 +0300

    Remove unnecessary specs and add grouping specs

--- spec/models/grouping_spec.rb
+++ spec/models/grouping_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Grouping, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:example) do
+    @user = User.create(email: 'test@example.com', password: '123456')
+    @url = @user.urls.create(full_url: 'http://example.com')
+  end
+
+  it "doesn't validate if user doesn't exist" do
+    group = Grouping.new(url_id: @url.id)
+    expect(group.valid?).to be false
+  end
+
+  it "doesn't validate if url doesn't exist" do
+    group = Grouping.new(user_id: @user.id)
+    expect(group.valid?).to be false
+  end
+
+  it "validates if both url and user exist" do
+    group = Grouping.new(user_id: @user.id, url_id: @url.id)
+    expect(group.valid?).to be true
+  end
 end
